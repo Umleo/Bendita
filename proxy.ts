c5db82efe40a7f7cba06d987ad5ab9b5835f58a2
Umleo <leorafael9974@gmail.com>
2026-04-13 16:45:31 -0300

    corrigindo bug

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { auth } from './app/lib/auth'
+//import { auth } from './app/lib/auth'
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
